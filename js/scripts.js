0d424f9e05e201fa79185e9e3e7fd2a7bcd8304b
ajackonski <113149629+ajackonski@users.noreply.github.com>
2024-01-30 18:30:25 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poisonj'] }
-    {name: 'Charmander', height: 0.6, type: ['fire'] }
+    {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poisonj'] },
+    {name: 'Charmander', height: 0.6, type: ['fire'] },
     {name: 'Squirtle', height: 0.5, type: ['water']}
-]
+];
