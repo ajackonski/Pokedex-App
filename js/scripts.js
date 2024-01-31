aa17285ebacea4f7212d7d13d3bed10c82054aed
ajackonski <113149629+ajackonski@users.noreply.github.com>
2024-01-30 18:29:40 -0600

    removed existing code and replaced it with the beginning of my pokemon list array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello World');
-let faveFood = 'Burritos';
-document.write(faveFood)
+let pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poisonj'] }
+    {name: 'Charmander', height: 0.6, type: ['fire'] }
+    {name: 'Squirtle', height: 0.5, type: ['water']}
+]
